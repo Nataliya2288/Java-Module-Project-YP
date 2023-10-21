7483df18366da33ee84b169fd84b155532dde159
nataliya <ameliya2288@yandex.ru>
2023-10-21 10:11:26 +0300

    Коммит 1.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,27 @@
-
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static int Guests;
+    static Scanner scanner = new Scanner(System.in);
+
+    public static int countOfGuests() {
+        System.out.println("На сколько гостей необходимо разделить счет?");
+        while (true) {
+            if (scanner.hasNextInt()) {
+                int i = scanner.nextInt();
+                if (i == 1) {
+                    System.out.println("Для одного гостя разбивать счёт нет необходимости. Введите другое число гостей");
+                } else if (i < 1) {
+                    System.out.println("Платить некому.Введите другое число гостей");
+                } else {
+                    Guests = i;
+                    break;
+                }
+            } else {
+                System.out.println("Введите целое число гостей");
+                scanner.nextLine();
+            }
+        }
+        return Guests;
     }
 }
+
